550247d0114a46741ac21618679536c4bc7e76eb
Gil <gilifon@gmail.com>
2016-04-25 14:47:22 +0300

    Checkin #1

---
+++ App/viewmodels/shell.js
@@ -0,0 +1,25 @@
+﻿define(['plugins/router', 'durandal/app'], function (router, app) {
+
+    
+    var selectedSubMenu = ko.observable('');
+    var selectedMainMenu = ko.observable('main');
+    var version = app.version;
+    
+    return {
+        selectedSubMenu: selectedSubMenu,
+        selectedMainMenu: selectedMainMenu,
+        version: version,
+        router: router,
+        activate: function () {
+            router.map([
+                { route: 'Dashboard', title: 'Dashboard', moduleId: 'viewmodels/dashboard', nav: true },
+                { route: '', title: 'Sections', moduleId: 'viewmodels/sections', nav: true }
+            ]).buildNavigationModel();
+
+            return router.activate();
+        },
+        compositionComplete: function () {
+            
+        }
+    };
+});
