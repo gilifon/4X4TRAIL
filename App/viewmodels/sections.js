4787a603be78e76a4490c583bb2e22d45519f429
Gil <gilifon@gmail.com>
2016-04-26 15:22:28 +0300

    added award section

--- App/viewmodels/sections.js
+++ App/viewmodels/sections.js
@@ -2,11 +2,40 @@
 
     var shell = require('viewmodels/shell');
 
+    //properties
+    this.linkList = ko.observableArray();
+    this.callsign = ko.observable('');
+
+    this.GetWokedSections = function () {
+        $.ajax({
+            type: "POST",
+            url: "./Server/GetWokedSections.php",
+            data: { 'info': { 'call': callsign() } }
+        }).done(function (data) {
+            linkList(data);
+            
+        }).error(function (xhr, ajaxOptions, thrownError) {
+            //alert(jQuery.parseJSON(xhr.responseText).error);
+            linkList([]);
+        });
+    }
+
+    this.IsSectionExist = function(section)
+    {
+        var queryResult = Enumerable.From(linkList).Where(function (x) { return x.section == section }).ToArray();
+        return queryResult.length;
+    }
+    
     var vm = {
         activate: function () {
             //shell.selectedSubMenu('ham');
             shell.selectedMainMenu('sections');
-        }
+        },
+        compositionComplete: function () {
+            
+        },
+        linkList: linkList,
+        callsign: callsign
     };
 
     //Note: This module exports a function. That means that you, the developer, can create multiple instances.
